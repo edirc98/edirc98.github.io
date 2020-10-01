3a00f1ed58bc179b0cc045442444fc5ac616d39f
edirc98 <edirc98@gmail.com>
2020-10-01 12:50:51 +0200

    Dist version ready

--- js/index.js
+++ js/index.js
@@ -3,6 +3,14 @@
     * Copyright 2013-2020 Start Bootstrap
     * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
     */
+
+    var typed = new Typed('#typed',{
+                    stringsElement: '#typed-strings',
+                    backSpeed: 40,
+                    typeSpeed: 40,
+                    loop: true
+                  });
+    
     (function ($) {
 
     "use strict"; // Start of use strict
